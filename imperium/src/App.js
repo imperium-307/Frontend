201570f33de5f85c993062ad9121f9d3c2ed2301
jszar <jszarzyn@purdue.edu>
2019-02-10 21:22:29 -0500

    Login Page

--- imperium/src/App.js
+++ imperium/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>Login</h1>
+        <form>
+          <label>
+            Username:
+            <br/>
+            <input type="text" name="username" />
+            <br/>
+            Password:
+            <br/>
+            <input type="text" name="password" />
+          </label>
+        </form>
+        <button>Submit</button>
+        <button>Create Account</button>
+        <br/>
+        <a href="https://www.google.com">Forgot Password?</a>
         </header>
       </div>
     );
