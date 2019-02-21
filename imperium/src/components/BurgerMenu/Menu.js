447ea6847fad8b2689fb6ce44a119f074719b492
jszar <jszarzyn@purdue.edu>
2019-02-21 12:15:53 -0500

    Home is now hidden when not signed in

--- imperium/src/components/BurgerMenu/Menu.js
+++ imperium/src/components/BurgerMenu/Menu.js
@@ -11,7 +11,7 @@ function logout() {
 export default ({ close }) => (
   <div className="menu">
     <ul>
-			{ localStorage.getItem('token') === null && 
+			{ localStorage.getItem('token') === null &&
 				<li onClick={close}>
 					<Link to={ROUTES.SIGN_IN}>Sign In</Link>
 				</li>
@@ -19,20 +19,22 @@ export default ({ close }) => (
       <li onClick={close}>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
+			{ localStorage.getItem('token') != null &&
       <li onClick={close}>
         <Link to={ROUTES.HOME}>Home</Link>
       </li>
-			{ localStorage.getItem('token') != null && 
+			}
+			{ localStorage.getItem('token') != null &&
       	<li onClick={close}>
         	<Link to={ROUTES.ACCOUNT}>Account</Link>
       	</li>
 			}
-			{ localStorage.getItem('token') != null && 
+			{ localStorage.getItem('token') != null &&
 				<li onClick={close}>
 					<Link to={ROUTES.MATCHES}>Matches</Link>
 				</li>
 			}
-			{ localStorage.getItem('token') != null && 
+			{ localStorage.getItem('token') != null &&
 				<li onClick={close}>
 					<Link onClick={logout} to={ROUTES.LANDING}>Sign Out</Link>
 				</li>
