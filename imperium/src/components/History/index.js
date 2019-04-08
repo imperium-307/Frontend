d1e759678cdf20ef377227008598818a7ed7986b
CJ Enright <cenrigh@purdue.edu>
2019-04-07 20:46:17 -0400

    Style clean up and history fixes

--- imperium/src/components/History/index.js
+++ imperium/src/components/History/index.js
@@ -2,41 +2,26 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
-//Routing might be broken
-// const HistoryPage = () => (
-//   <div>
-//     <h1>History</h1>
-//     <HistoryList />
-//   </div>
-// );
-
-const styles = {
-  fontFamily: "arial",
-  textAlign: "center",
-  marginTop: "40px",
-  color: "#421CE8"
-};
-
 const HistoryPage = () => (
-  <div style={styles}>
-  <style>{'body { background-color: #DBDAE1; }'}</style>
-    <h1>History</h1>
-		<HistoryList />
-  </div>
+	<div>
+	<h1>History</h1>
+	<HistoryList />
+	</div>
 )
 
 var INITIAL_STATE = {
-	history: null,
-  persona: null,
+	history: {},
+	persona: "",
+	isLoading: true,
 	error: null,
 };
 
 class HistoryList extends Component{
-  constructor(props){
-    super(props);
-    this.state = { ...INITIAL_STATE };
+	constructor(props){
+		super(props);
+		this.state = { ...INITIAL_STATE };
 
-    fetch("http://localhost:3000/api/user", {
+		fetch("http://localhost:3000/api/user", {
 			body: JSON.stringify({
 				token: localStorage.getItem('token')
 			}),
@@ -52,46 +37,41 @@ class HistoryList extends Component{
 				return res.json()
 			})
 			.then((res) => {
-        this.setState({
-          history: res.history,
-          persona: res.persona,
-        });
+				this.setState({
+					history: res.history,
+					persona: res.persona,
+					isLoading: false
+				});
 			})
 			.catch(error => {
 				this.setState({ error });
 			});
-  }
+	}
 
 
-  render(){
-    console.log(this.state.history);
-    if (!this.state || !this.state.history) {
-      return (
-        <div>
-          <p>Loading...</p>
-        </div>
-      );
-    }
-    else{
-      const { history, peronsa } = this.state;
-      if (this.state.persona === "student") {
-        let body = "You have no history";
-        if (this.state.history){
-          body = this.state.history.map((d,i) => {
-            return (<p>You {this.state.history[i].action}d {this.state.history[i].data} on {this.state.history[i].date}</p>)
-          })
-        }
-        else {
-          body = <h1>sad pizza time</h1>
-          //should never print
-        }
-        return (
-          <h1>{body}</h1>
-        )
-      }
+	render() {
+		const { history, peronsa } = this.state;
 
-    }
-  }
+		if (this.state.isLoading) {
+			return (
+				<div>
+				<p>Loading...</p>
+				</div>
+			);
+		} else {
+			if (this.state.history) {
+				return this.state.history.map((d, i) => {
+					return (
+						<p>You {this.state.history[i].action}d {this.state.history[i].data} on {this.state.history[i].date}</p>
+					)
+				})
+			} else {
+				return (
+					<h1>You have no history</h1>
+				)
+			}
+		}
+	}
 }
 
 export default HistoryPage;
