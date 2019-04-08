5355ae6d3016637bc76e7626158db20b1762fe12
jszar <jszarzyn@purdue.edu>
2019-04-08 01:05:46 -0400

    Changed previous commit a bit

--- imperium/src/components/Favorite/index.js
+++ imperium/src/components/Favorite/index.js
@@ -4,10 +4,26 @@ import { compose } from 'recompose';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
-const FavoritePage = () => (
-  <div>
-    <h1>My Favorites</h1>
-  </div>
-);
 
-export default FavoritePage;
+var INITIAL_STATE = {
+	error: null,
+};
+
+class FavoritePage extends Component{
+  constructor(props) {
+		super(props);
+
+		this.state = { ...INITIAL_STATE };
+	}
+  render() {
+    return (
+      <div>
+        <h1>My Favorites</h1>
+      </div>
+    )
+  }
+}
+
+export default compose(
+  withRouter,
+)(FavoritePage);
