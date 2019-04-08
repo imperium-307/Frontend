3c72a87db4aa65514315502097946ff6d51a72ee
jszar <jszarzyn@purdue.edu>
2019-04-08 01:08:03 -0400

    Changed how chat page was initally set up

--- imperium/src/components/Chat/index.js
+++ imperium/src/components/Chat/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
-const ChatPage = () => (
-  <div>
-    <h1>Chat</h1>
-  </div>
-);
 
-export default ChatPage;
+var INITIAL_STATE = {
+	error: null,
+};
+
+class ChatPage extends Component{
+  constructor(props) {
+		super(props);
+
+		this.state = { ...INITIAL_STATE };
+	}
+  render() {
+    return (
+      <div>
+        <h1>Chat</h1>
+      </div>
+    )
+  }
+}
+
+export default compose(
+  withRouter,
+)(ChatPage);
