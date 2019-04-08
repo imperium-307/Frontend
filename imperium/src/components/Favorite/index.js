cafbca180849ffb4bc16df239032329aa9ee6411
jszar <jszarzyn@purdue.edu>
2019-04-08 12:56:40 -0400

    Favorite Page will filter history and show your favorites

--- imperium/src/components/Favorite/index.js
+++ imperium/src/components/Favorite/index.js
@@ -6,6 +6,8 @@ import * as ROUTES from '../../constants/routes';
 
 
 var INITIAL_STATE = {
+	history: "",
+	isLoading: true,
 	error: null,
 };
 
@@ -14,11 +16,59 @@ class FavoritePage extends Component{
 		super(props);
 
 		this.state = { ...INITIAL_STATE };
+
+		fetch("http://localhost:3000/api/user", {
+			body: JSON.stringify({
+				token: localStorage.getItem('token')
+			}),
+			cache: 'no-cache',
+			credentials: 'same-origin',
+			headers: {
+				'content-type': 'application/json'
+			},
+			mode: 'cors',
+			method: 'POST'
+		})
+			.then((res) => {
+				return res.json()
+			})
+			.then((res) => {
+				this.setState({
+					history: res.history,
+					isLoading: false,
+				});
+			})
+			.catch(error => {
+				this.setState({ error });
+			});
 	}
   render() {
     return (
       <div>
-        <h1>My Favorites</h1>
+			<h1>My Favorites</h1>
+			{(() => {
+				if (this.state.isLoading) {
+					return (
+						<div>
+						<p>Loading...</p>
+						</div>
+					);
+				} else {
+					if (this.state.history) {
+						return this.state.history.map((d, i) => {
+							if (this.state.history[i].action === "favorite"){
+							return (
+								<p>You {this.state.history[i].action}d {this.state.history[i].data} on {new Date(this.state.history[i].date).toLocaleDateString("en-US")} at {new Date(this.state.history[i].date).toLocaleTimeString("en-US")}</p>
+							)
+						}
+						})
+					} else {
+						return (
+							<h1>You have no history</h1>
+						)
+					}
+				}
+			})()}
       </div>
     )
   }
